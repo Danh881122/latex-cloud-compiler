d841a784bd831d1b3407cbbba85bf12e0f5d73cc
Danh881122 <trucdanh1988@gmail.com>
2025-11-01 00:49:05 +0700

    Update server.py

--- server.py
+++ server.py
@@ -40,4 +40,6 @@ def compile_tex():
     return send_file(pdf_path, mimetype="application/pdf")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
